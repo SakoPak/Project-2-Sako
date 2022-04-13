449cde669b61f17313cbc9f90d128ed9988dff9f
SakoPak <sako.taya@gmail.com>
2022-04-12 20:47:04 -0700

    Add Auth api
    Created initial structure and framework

---
+++ auth/api.js
@@ -0,0 +1,48 @@
+'use strict'
+const store = require('../store.js')
+const config = require('../config.js')
+
+const signUp = function (data) {
+  console.log(store)
+  return $.ajax({
+    method: 'POST',
+    url: config.apiUrl + '/sign-up',
+    data
+  })
+}
+
+const signIn = function (data) {
+  return $.ajax({
+    method: 'POST',
+    url: config.apiUrl + '/sign-in',
+    data
+  })
+}
+
+const signOut = function () {
+  return $.ajax({
+    method: 'DELETE',
+    url: config.apiUrl + '/sign-out',
+    headers: {
+      Authorization: 'Bearer ' + store.user.token
+    }
+  })
+}
+
+const changePassword = function (data) {
+  return $.ajax({
+    method: 'PATCH',
+    url: config.apiUrl + '/change-password',
+    data,
+    headers: {
+      Authorization: 'Bearer ' + store.user.token
+    }
+  })
+}
+
+module.exports = {
+  signUp,
+  signIn,
+  signOut,
+  changePassword
+}
