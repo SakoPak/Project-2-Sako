6353ebfb74e97373bdced74abd149da5ebd7105c
SakoPak <sako.taya@gmail.com>
2022-04-13 21:43:34 -0700

    Add sign in curl

---
+++ curl-scripts/user/sign-in.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# sh curl-scripts/user/sign-in.sh
+
+EMAIL="sako@angel.com"
+PASSWORD="hope"
+
+
+API="http://localhost:4741"
+URL_PATH="/sign-in"
+
+curl "${API}${URL_PATH}" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --data '{
+    "credentials": {
+      "email": "'"${EMAIL}"'",
+      "password": "'"${PASSWORD}"'"
+    }
+  }'
+
+echo
