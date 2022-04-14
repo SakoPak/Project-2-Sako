37030029ece67c884f8359defd603196c7c16b5d
SakoPak <sako.taya@gmail.com>
2022-04-13 21:31:52 -0700

    Add delete curl script for dog resource

---
+++ curl-scripts/dogProfiles/delete-profile.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# sh curl-scripts/dogProfile/delete-profile.sh
+
+TOKEN="1fc71b8ed525c6a0dddf7cadeef9eea4"
+ID="6255d6a4cd761170534ce41c"
+
+API="http://localhost:4741"
+URL_PATH="/dogProfiles"
+
+curl "${API}${URL_PATH}/${ID}" \
+  --include \
+  --request DELETE \
+  --header "Authorization: Bearer ${TOKEN}"
+
+echo
