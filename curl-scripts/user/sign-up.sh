2c8d533d357f7d66f285bae1abc29c86bbf5d792
SakoPak <sako.taya@gmail.com>
2022-04-13 21:44:31 -0700

    Add sign up curl

---
+++ curl-scripts/user/sign-up.sh
@@ -0,0 +1,22 @@
+# sh curl-scripts/user/sign-up.sh
+
+EMAIL="sako@angel.com"
+PASSWORD="hope"
+
+
+API="http://localhost:4741"
+URL_PATH="/sign-up"
+
+curl "${API}${URL_PATH}" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --data '{
+    "credentials": {
+      "email": "'"${EMAIL}"'",
+      "password": "'"${PASSWORD}"'",
+      "password_confirmation": "'"${PASSWORD}"'"
+    }
+  }'
+
+echo
