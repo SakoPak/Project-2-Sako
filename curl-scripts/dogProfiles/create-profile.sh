0c2d5544eda925da6cb0a497efdd5111d9f84050
SakoPak <sako.taya@gmail.com>
2022-04-13 21:30:49 -0700

    Add Create Curl script for dog resource

---
+++ curl-scripts/dogProfiles/create-profile.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# sh curl-scripts/dogProfiles/create-profile.sh
+
+# NAME="Bruno"
+# AGE="6 years old"
+# GENDER="Male"
+# TYPE="Boxer"
+# NOTES="He likes bacon."
+# TOKEN="bc739d6ffef993f1a1c759731b607a5d"
+
+
+API="http://localhost:4741"
+URL_PATH="/dogProfiles"
+
+curl "${API}${URL_PATH}" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Bearer ${TOKEN}" \
+  --data '{
+    "dog":{
+      "name": "'"${NAME}"'",
+      "age": "'"${AGE}"'",
+      "gender": "'"${GENDER}"'",
+      "type": "'"${TYPE}"'",
+      "notes": "'"${NOTES}"'"
+      }
+  }'
+
+echo
+
