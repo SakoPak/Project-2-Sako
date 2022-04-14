63e903e2967b88059eaac99cce723d862369dcef
SakoPak <sako.taya@gmail.com>
2022-04-13 21:33:52 -0700

    Add update curl scripts for dog resource

---
+++ curl-scripts/dogProfiles/update-profile.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# sh curl-scripts/dogProfile/update-profile.sh
+
+# NAME="Snowball"
+# AGE="2 years old"
+# GENDER="Male"
+# TYPE="Terrier"
+# NOTES="Scared of cars"
+# TOKEN="bc739d6ffef993f1a1c759731b607a5d"
+# ID="62572824256007a90d00d269"
+
+
+API="http://localhost:4741"
+URL_PATH="/dogProfile"
+
+curl "${API}${URL_PATH}/${ID}" \
+  --include \
+  --request PATCH \
+  --header "Content-Type: application/json" \
+--header "Authorization: Bearer ${TOKEN}" \
+--data '{
+    "dog": {
+      "name": "'"${NAME}"'",
+      "age": "'"${AGE}"'",
+      "gender": "'"${GENDER}"'",
+      "type": "'"${TYPE}"'",
+      "notes": "'"${NOTES}"'"
+    }
+  }'
+
+echo
