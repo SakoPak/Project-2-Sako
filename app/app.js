924d2dc03fc5030215a82e192b661e9341b81bf4
SakoPak <sako.taya@gmail.com>
2022-04-13 21:35:15 -0700

    Add app.js with initial event listeners

--- app/app.js
+++ app/app.js
@@ -1,9 +1,18 @@
 // use require with a reference to bundle the file and use it in this file
-// const example = require('./example')
+const authEvents = require('./auth/events.js')
+const dogEvents = require('./dogAuth/events.js')
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
 $(() => {
-  // your JS code goes here
+  // $('#sign-out-message').hide()
+  $('#signUp').on('submit', authEvents.onSignUp)
+  $('#signIn').on('submit', authEvents.onSignIn)
+  $('#signOut').on('click', authEvents.onSignOut)
+  $('#changePassword').on('submit', authEvents.onChangePassword)
+  $('#create-profile').on('submit', dogEvents.onCreateProfile)
+  $('#create-profile-message').on('submit', dogEvents.onCreateProfile)
+  $('#all-profiles').on('click', dogEvents.onIndexProfiles)
+  $('#view-profile').on('click', dogEvents.onViewProfile)
 })
