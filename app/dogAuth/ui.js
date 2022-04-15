3fb4de879f3a65f7dbc5ecedbe62d7cf9b07548e
SakoPak <sako.taya@gmail.com>
2022-04-14 21:22:19 -0700

    updated Dog profile ui file

--- app/dogAuth/ui.js
+++ app/dogAuth/ui.js
@@ -13,14 +13,15 @@ const onCreateProfileFailure = function () {
 }
 
 const onViewProfileSuccess = function (response) {
+  console.log(response)
   const profileHtml = `
                       <div>
-                        '<li>' + ${dog.name} + '</li>'
-                        <p>${dog.name}</p>
-                        <p>${dog.gender}</p>
-                        <p>${dog.age}</p>
-                        <p>${dog.type}</p>
-                        <p>${dog.notes}</p>
+                        '<li>' + ${response.dog.name} + '</li>'
+                        <p>${response.dog.name}</p>
+                        <p>${response.dog.gender}</p>
+                        <p>${response.dog.age}</p>
+                        <p>${response.dog.type}</p>
+                        <p>${response.dog.notes}</p>
                       </div>
                     `
   $('#view-profile').html(profileHtml)
@@ -32,57 +33,91 @@ const onViewProfileFailure = function () {
 }
 
 const onIndexProfileSuccess = function (response) {
+  const indexProfiles = response.dog
+  console.log(response)
+
   let profilesHtml = ''
 
-  response.dogProfiles.forEach((book) => {
-    // booksHtml += '<li>' + book.title + '</li>'
-    // booksHtml = booksHtml + '<li>' + book.title + '</li>'
-
+  indexProfiles.forEach(dog => {
     profilesHtml += `
-                      <div>
-                      <h3>${dog.name}</h3>
-                      '<li>' + ${dog.name} + '</li>'
-                        <p>${dog.name}</p>
-                        <p>${dog.gender}</p>
-                        <p>${dog.age}</p>
-                        <p>${dog.type}</p>
-                        <p>${dog.notes}</p>
+                      <div id=${dog._id}>
+                    
+                       <h3>Name: ${dog.name}</h3> 
+                        <div>
+                          <p>Gender: ${dog.gender}</p>
+                          <p>Birthday: ${dog.age}</p> 
+                          <p>Type: ${dog.type}</p>
+                          <p>Notes: ${dog.notes}</p> 
+                        </div>
+                    
                       </div>
-                      <button class="delete-profile" data-id=${dog._id}>Delete Profile</button>
-                        <button class="edit-profile" data-id=${dog._id}>Edit Profile</button>
+
+                    <section class="update-profile-list">
+                    <h3>Need to update ${dog.name}'s profile? Fill out the form and click 'Update'! </h3> 
+                    
+                      <form class="update-profile-list" data-id=${dog._id}>
+                            <input name="dog[name]" type="text" placeholder="Dog Name" required>
+                            <input name="dog[age]" type="text" placeholder="00/00/00" required>
+                            <input name="dog[gender]" type="text" placeholder="Gender" required>
+                            <input name="dog[type]" type="text" placeholder="Labrador" required>
+                            <input name="dog[notes]" type="text" placeholder="He likes to swim!" required>
+
+                        </form>
+                        <button type="submit" class="update-profile-list" data-id=${dog._id}>Update ${dog.name}'s profile </button>
+                        <button class="delete-profile" data-id=${dog._id}>Delete ${dog.name}'s Profile</button>
                         
+                    </section>
+                        <br>
                     `
   })
-  $('#profiles-display').html(profilesHtml)
-  $('form').trigger('reset')
-  $('form').trigger('update')
-  $('#error-message').text('')
+  $('#view-all-profiles').html(profilesHtml)
+  // $('form').trigger('reset')
+  // $('form').trigger('update')
+  // $('#error-message').text('')
 }
 
-const onUpdateProfileSuccess = function () {
-  const bookHtml = `
-    <div>
-        <form class="update-book-list" data-id=${dog._id}>
-                            <input name="book[title]" type="text" placeholder="Book Title">
-                            <input name="book[author]" type="text" placeholder="Book Author">
-                            <button type="submit">Update book</button>
-                        </form>
-       </div>
-    `
+const onIndexProfileFailure = function () {
+  $('#error-message').text('Failure while trying to view all profiles.')
+}
+
+const onUpdateProfileSuccess = function (response) {
+  $('#profile-update-message').html('Edit successful!')
+  $('#view-all-profiles').html('Profiles have changed! Click "All Profiles" again to view all dog profiles.')
+
+  $('#profile-update-message').addClass('success')
+  setTimeout(() => {
+    $('#profile-update-message').html('')
+    $('#profile-update-message').removeClass('success')
+  }, 5000)
+
   $('form').trigger('reset')
 }
-$('#books-display').html('Success. Book updated!')
 
 const onUpdateProfileFailure = function () {
-  $('#error-message').text('Failure while trying to update book')
+  $('#error-message').text('Failure while trying to update profile.')
+}
+
+const onDeleteSuccess = function () {
+  $('#profile-delete-message').html('Profile successfully deleted.')
+  $('#view-all-profiles').html('Profiles have changed! Click "All Profiles" again to view all dog profiles.')
+  $('#profile-delete-message').addClass('success')
+
+  setTimeout(() => {
+    $('#profile-delete-message').html('')
+    $('#profile-delete-message').removeClass('success')
+  }, 5000)
+
+  $('form').trigger('reset')
 }
 
 module.exports = {
   onCreateProfileSuccess,
   onCreateProfileFailure,
   onIndexProfileSuccess,
+  onIndexProfileFailure,
   onViewProfileSuccess,
   onViewProfileFailure,
   onUpdateProfileSuccess,
-  onUpdateProfileFailure
+  onUpdateProfileFailure,
+  onDeleteSuccess
 }
