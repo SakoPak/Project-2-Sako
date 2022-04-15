4841aa77383d67692c7ff758128178f8f757c7b4
SakoPak <sako.taya@gmail.com>
2022-04-15 12:04:58 -0700

    Edited functions

--- app/app.js
+++ app/app.js
@@ -7,6 +7,19 @@ const dogEvents = require('./dogAuth/events.js')
 
 $(() => {
   $('#sign-in-container').hide()
+  $('#go-to-sign-in').on('click', authEvents.onShowSignIn)
+  $('#home-page').hide()
+  $('#update-form').hide()
+  $('#all-profiles-page').hide()
+  $('#sign-out-pwForm').hide()
+  $('#passwordForm').hide()
+  $('#passwordFormBtn').hide()
+  $('#go-homeBtn').hide()
+
+  $('#go-to-add-profile').on('click', authEvents.onShowUpdateForm)
+  $('#go-to-all-profiles').on('click', authEvents.onShowAllProfiles)
+  $('#passwordFormBtn').on('click', authEvents.onShowPWForm)
+  $('#go-homeBtn').on('click', authEvents.onShowHomePage)
 
   $('#signUp').on('submit', authEvents.onSignUp)
   $('#signIn').on('submit', authEvents.onSignIn)
@@ -15,9 +28,14 @@ $(() => {
   $('#createProfile').on('submit', dogEvents.onCreateProfile)
   $('#create-profile-message').on('submit', dogEvents.onCreateProfile)
   $('#all-profiles').on('click', dogEvents.onIndexProfiles)
+  $('#view-all-profiles').on('submit', '.update-profile-list', dogEvents.onUpdateProfile)
+  $('#view-all-profiles').on('click', '.delete-profile',
+    dogEvents.onDeleteProfile
+  )
+
   // $('.update-profile-list').hide()
-  $('.update-profile-list').on('click', dogEvents.onDeleteProfile)
-  $('.delete-profile').on('click', dogEvents.onUpdateProfile)
+  // $('.update-button').on('submit', dogEvents.onUpdateProfile)
+  // $('.delete-profile').on('click', dogEvents.onDeleteProfile)
 })
 
 // $('#view-profile').on('click', dogEvents.onViewProfile)
