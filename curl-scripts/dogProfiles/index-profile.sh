0829c820c1175d23cba5445c16178d5536b10c37
SakoPak <sako.taya@gmail.com>
2022-04-17 23:57:33 -0700

    Add production url

--- curl-scripts/dogProfiles/index-profile.sh
+++ curl-scripts/dogProfiles/index-profile.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # sh curl-scripts/dogProfile/index-profile.sh
 
-API="http://localhost:4741"
+API="https://cryptic-shore-49973.herokuapp.com"
 URL_PATH="/dogProfiles"
 
 curl "${API}${URL_PATH}" \
